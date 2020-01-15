6ac5725aa83201bc67db63d31c07ff7e16f099ee
Dallin Clayton <dallin.clayton@gmail.com>
2020-01-14 23:04:37 -0700

    Basic prototype and roadmap

--- README.md
+++ README.md
@@ -4,15 +4,28 @@ Turning vim shortcuts into verbal coding for hands-free programming
 ## requirements
 SpeechRecognition python package
 PyAudio for microphone
+PocketSphinx
+    I had to manually download a precompiled wheel for pocketsphinx here:
+    https://www.lfd.uci.edu/~gohlke/pythonlibs/#pocketsphinx
 
 CMUSphinx is one of the best offline options
     Pocket sphinx package
+I'll have to look into kaldi at some point
 TODO Might actually have to use vosk/kaldi because the group that made sphinx is working on that one now and it is maintained, unlike sphinx, although the speechrecognition tutorial is recent enough that maybe it is fine. It looks like the pocketsphinx is working fine so far
-Had to manually download a precompiled wheel for pocketsphinx here:
-https://www.lfd.uci.edu/~gohlke/pythonlibs/#pocketsphinx
-
 I might just have to try the old one and switch to the new one if need be
 
 
-this tutorial was very helpful:
+this tutorial was very helpful (And I probably copied some code from it):
 https://realpython.com/python-speech-recognition/
+
+seems to work better with a headset, go figure
+
+
+# TODOS
+* Make a way to add a custom list of words to recognize as other words (for example, saying "yank" almost always leads to the word "yet" instead)
+* Customizable escape phrases (kind of like remapping jj to escape input mode)
+* some way to map arbitrary phrases to certain keybindings (i.e. saying "go to top" would result in "gg")
+* figure out how to search the phrase for instances of the word "space" (or another predefined word), also things like "underscore", "dot", etc. Hopefully just make this super customizable (replace phrase x with symbol y)
+* figure out how to make logical names for variables (like figure out where to put camelCase or snake_case for a name instead of with spaces)
+* pause functionality (say "pause" to stop taking input and "resume" to continue taking input, so that a person using it can stop to talk to people and stuff like that)
+* (super in the future) make it so you can do most things on your computer with it (alt-tabbing, switching workspaces, and other hacks like that)
--- testing.py
+++ testing.py
@@ -1,13 +1,52 @@
 import speech_recognition as sr
-print(sr.__version__)
+import keyboard
 
-r = sr.Recognizer()
+# TODO make a way to add a custom list of words to recognize as other words (for example, saying "yank" almost always leads to the word "yet" instead)
+def get_phrase_from_mic(recognizer, mic):
+    result = "#ERROR#"
+    if not isinstance(recognizer, sr.Recognizer):
+        print("Recognizer was not an instance of the Recognizer class")
+        return result
+    if not isinstance(mic, sr.Microphone):
+        print("Mic was not an instance of the Microphone class")
+        return result
+    try:
+        with mic as source:
+            r.adjust_for_ambient_noise(source)
+            print("Say something...")
+            audio_input = r.listen(source)
+            result = r.recognize_sphinx(audio_input)
+    except sr.UnknownValueError:
+        print("Speech couldn't be recognized")
+        return result
+    return result
 
-mic = sr.Microphone()
+if __name__ == "__main__":
+    mic = sr.Microphone()
+    r = sr.Recognizer()
+    while True:
+        phrase = get_phrase_from_mic(r, mic)
+        print(phrase)
+        if phrase == "#ERROR#":
+            continue
+        elif phrase == "input":
+            keyboard.press_and_release("i")
+        elif phrase == "escape":
+            keyboard.press_and_release("esc")
+        elif phrase.lower() == "exit":
+            print("Well bye then")
+            break
+        else:
+            keyboard.write(phrase)
 
-with mic as source:
-    print("Say something...")
-    r.adjust_for_ambient_noise(source)
-    audio_input = r.listen(source)
-    result = r.recognize_sphinx(audio_input)
-    print(result)
+#n thatskatand this could be workingmaybe it is worthe a#ERROR#tenjaybutupbuild upgo upi don't know what to do although you aredoes this work betterc. n. but it does work betterit seems like it does work betterwith so little bitat least a little bitspace
+        
+
+# >>> with harvard as source:
+# ...     audio1 = r.record(source, duration=4)
+# ...     audio2 = r.record(source, duration=4)
+# ...
+# >>> r.recognize_google(audio1)
+# 'the stale smell of old beer lingers'
+# >>> r.recognize_google(audio2)
+# 'it takes heat to bring out the odor a cold dip'
