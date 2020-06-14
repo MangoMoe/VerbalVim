b9e1711e118dda6b4320c7e43bbc21c4ef42b684
Dallin Clayton <dallin.clayton@gmail.com>
2020-06-14 12:04:42 -0600

    Some testing/progress

--- testing.py
+++ testing.py
@@ -4,6 +4,7 @@ import keyboard
 # TODO make a way to add a custom list of words to recognize as other words (for example, saying "yank" almost always leads to the word "yet" instead)
 def get_phrase_from_mic(recognizer, mic):
     result = "#ERROR#"
+    # TODO are these really necessary checks?
     if not isinstance(recognizer, sr.Recognizer):
         print("Recognizer was not an instance of the Recognizer class")
         return result
@@ -21,6 +22,7 @@ def get_phrase_from_mic(recognizer, mic):
         return result
     return result
 
+# TODO I forgot how long the microphone records at a time, can you control this?
 if __name__ == "__main__":
     mic = sr.Microphone()
     r = sr.Recognizer()
@@ -31,15 +33,22 @@ if __name__ == "__main__":
             continue
         elif phrase == "input":
             keyboard.press_and_release("i")
-        elif phrase == "escape":
+        elif phrase == "escapade": # TODO it apparently uses context so saying "escape" by itself rarely works
             keyboard.press_and_release("esc")
+        elif "camel case" in phrase.lower():
+            # Remove camel case from phrase
+            # convert phrase to camel case
+            # TODO should we also have like an "end camelcase" thing? like also a voice command to end it?
+            pass
         elif phrase.lower() == "exit":
             print("Well bye then")
             break
         else:
+            phrase = phrase + " " # Adding a space at the end
             keyboard.write(phrase)
 
 #n thatskatand this could be workingmaybe it is worthe a#ERROR#tenjaybutupbuild upgo upi don't know what to do although you aredoes this work betterc. n. but it does work betterit seems like it does work betterwith so little bitat least a little bitspace
+#all ofthis guythat did thatohwhat is it working
         
 
 # >>> with harvard as source:
